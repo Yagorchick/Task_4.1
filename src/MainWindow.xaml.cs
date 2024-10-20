06c4d864f54a4dc4520870f6b6dc32174198063c
Egor <chrissuco537@gmail.com>
2024-10-20 23:17:48 +0300

    Task

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_4_Iwanov_Egor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Window Window1 = new Window1();
+            Window1.Closed += (s, args) => this.Close();
+            Window1.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Window Window2 = new Window2();
+            Window2.Closed += (s, args) => this.Close();
+            Window2.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Window Window3 = new Window3();
+            Window3.Closed += (s, args) => this.Close();
+            Window3.Show();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Window Window4 = new Window4();
+            Window4.Closed += (s, args) => this.Close();
+            Window4.Show();
+        }
+    }
+}
